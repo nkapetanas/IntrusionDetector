e55f0223f2c29e66a313b683c8d44a7a1d9598e3
Nick Kapetanas <nkapetanas@hotmail.com>
2018-03-23 00:41:52 +0200

    fixed output of word2vec file

--- neural_network.py
+++ neural_network.py
@@ -2,13 +2,14 @@ from __future__ import print_function
 
 import lit as lit
 import numpy as np
-from pyspark.shell import sqlContext
+from pyspark.shell import sqlContext, sc
 from pyspark.sql import SparkSession, types, dataframe
 #from pyspark.ml.feature import Word2Vec
 from pyspark.mllib.feature import Word2Vec
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.ml.feature import Bucketizer
 from pyspark.sql import functions as func
+from pyspark import SQLContext
 import pickle
 import csv
 
@@ -60,7 +61,7 @@ schema = StructType([
     StructField("dst_host_srv_serror_rate", StringType(), True),
     StructField("dst_host_rerror_rate", StringType(), True),
     StructField("dst_host_srv_rerror_rate", StringType(), True),
-    #StructField("end_Signal", StringType(), True)
+    StructField("anws", StringType(), True)
 ])
 
 final_struc = StructType(fields = schema)
@@ -141,6 +142,23 @@ for index, answer in enumerate(answers):
 
 '''
 
+
+
+
+
+#sqlContext = SQLContext(sc)
+
+#logsTest_rdd = sc.textFile("data/kddcup.data_10_percent_corrected").map(lambda line: line.split(","))
+
+#Employee_df = logsTest_rdd.toDF(schema)
+
+
+
+#word2Vec = Word2Vec(vectorSize=3, minCount=0, inputCol="duration,protocol_type,service,flag,src_bytes,dst_bytes,land,wrong_fragment,urgent,hot,num_failed_logins,logged_in,num_compromised,root_shell,su_attempted,num_root,num_file_creations,num_shells,num_access_files,num_outbound_cmds,is_host_login,is_guest_login,count,srv_count,serror_rate,srv_serror_rate,rerror_rate,srv_rerror_rate,same_srv_rate,diff_srv_rate,srv_diff_host_rate,dst_host_count,dst_host_srv_count,dst_host_same_srv_rate,dst_host_diff_srv_rate,dst_host_same_src_port_rate,dst_host_srv_diff_host_rate,dst_host_serror_rate,dst_host_srv_serror_rate,dst_host_rerror_rate,dst_host_srv_rerror_rate, anws", outputCol="result")
+#model = word2Vec.fit(Employee_df)
+#model.getVectors().show()
+
+
 # for counting unique values
 # #lstemp = [log[22] for log in logs]
 #temp = list(set(lstemp))
@@ -152,12 +170,25 @@ def save_word2vec(vectors):
         # print(key)
         vecs_python[key] = list(vectors[key])
     print('The vocabulary size is:' + str(len(vecs_python)))
-    np.array(vecs_python).dump(open('array.npy', 'wb'))
+
+    #np.array(vecs_python).dump(open('array.npy', 'wb'))
+    a = np.array(vecs_python)
+    #np.set_printoptions(threshold=np.nan)
+    #print(a)
+    #for key in vectors.keys():
+        #print("The key is:", key, "and the values are:", vecs_python[key] )
+
+    word2vec_results_dict = {key : vecs_python[key] for key in vectors.keys()}
+
     #with open('word2vec_dict.txt', 'wb') as handle:
         #wr = csv.writer(handle, quoting=csv.QUOTE_ALL)
         #wr.writerow(vecs_python)
         #for item in vecs_python:
             #handle.write(item)
+    with open('dict.csv', 'wb') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in word2vec_results_dict.items():
+            writer.writerow([key, value])
     #return vecs_python
 
 
@@ -175,6 +206,10 @@ def Word2_vec():
 
     #documentDF_temp.show()
     # df = sqlContext.createDataFrame(logs_rdd, schema)
+    #listT = ["duration_bucketized_enc", "protocol_type", "service, flag", "src_bytes_enc", "dst_bytes_bucketized_enc", "land_enc, wrong_fragment_enc", "urgent_enc", "hot_enc", "num_failed_logins_enc", "logged_in_enc", "num_compromised_enc", "root_shell_enc", "su_attempted_enc", "num_root_enc", "num_file_creations_enc", "num_shells_enc", "num_access_files_enc", "num_outbound_cmds_enc", "is_host_login_enc", "is_guest_login_enc", "count_bucketized_enc", "srv_count_bucketized_enc", "serror_rate_bucketized_enc", "srv_serror_rate_bucketized_enc", "rerror_rate_bucketized_enc", "srv_rerror_rate_bucketized_enc", "same_srv_rate_bucketized_enc", "diff_srv_rate_bucketized_enc", "srv_diff_host_rate_enc", "dst_host_count_enc", "dst_host_srv_count_enc", "dst_host_same_srv_rate_enc", "dst_host_diff_srv_rate_enc", "dst_host_same_src_port_rate_enc", "dst_host_srv_diff_host_rate_enc", "dst_host_serror_rate_enc", "dst_host_srv_serror_rate_enc", "dst_host_rerror_rate_enc", "dst_host_srv_rerror_rate_enc"]
+    #word2Vec = Word2Vec(vectorSize=3, minCount=0, inputCol=(col for col in listT), outputCol="result")
+    #model = word2Vec.fit(dataframe_with_bucket)
+    #model.getVectors().show()
 
     #dataframe_bucked_map = dataframe_with_bucket.rdd.map(lambda x: (x['duration_bucketized_enc'], x['protocol_type'],x['service'], x['flag'],x['src_bytes_enc'], x['dst_bytes_bucketized_enc'], x['land_enc'], x['wrong_fragment_enc'],x['urgent_enc'], x['hot_enc'],x['num_failed_logins_enc'], x['logged_in_enc'],x['num_compromised_enc'], x['root_shell_enc'], x['su_attempted_enc'],x['num_root_enc'], x['num_file_creations_enc'], x['num_shells_enc'],x['num_access_files_enc'], x['num_outbound_cmds_enc'], x['is_host_login_enc'], x['is_guest_login_enc'], x['count_bucketized_enc'], x['srv_count_bucketized_enc'],x['serror_rate_bucketized_enc'], x['srv_serror_rate_bucketized_enc'], x['rerror_rate_bucketized_enc'], x['srv_rerror_rate_bucketized_enc'], x['same_srv_rate_bucketized_enc'], x['diff_srv_rate_bucketized_enc'],x['srv_diff_host_rate_enc'], x['dst_host_count_enc'], x['dst_host_srv_count_enc'], x['dst_host_same_srv_rate_enc'],x['dst_host_diff_srv_rate_enc'], x['dst_host_same_src_port_rate_enc'],x['dst_host_srv_diff_host_rate_enc'], x['dst_host_serror_rate_enc'], x['dst_host_srv_serror_rate_enc'], x['dst_host_rerror_rate_enc'],x['dst_host_srv_rerror_rate_enc']))
     #mtcars_map.take(5)
