ca16e8b61e41c03964a59670d3586037f533ee35
Nick Kapetanas <nkapetanas@hotmail.com>
2018-03-27 00:41:22 +0300

    code with errors but gives the plot, needs refactoring

--- neural_network.py
+++ neural_network.py
@@ -1,18 +1,26 @@
 from __future__ import print_function
 
-import lit as lit
 import numpy as np
-from pyspark.shell import sqlContext, sc
 from pyspark.sql import SparkSession, types, dataframe
 #from pyspark.ml.feature import Word2Vec
 from pyspark.mllib.feature import Word2Vec
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.ml.feature import Bucketizer
 from pyspark.sql import functions as func
-from pyspark import SQLContext
-import pickle
 import csv
+from matplotlib.ticker import NullFormatter
+from time import time
+from sklearn.manifold import TSNE
+import sys
+import codecs
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
+import seaborn as sns
+import pickle
 
+
+# Random state.
+RS = 20150101
 
 spark = SparkSession.builder.appName('ids').getOrCreate()
 
@@ -67,6 +75,28 @@ schema = StructType([
 final_struc = StructType(fields = schema)
 df = spark.read.csv('data/kddcup.data_10_percent_corrected',schema=final_struc, inferSchema = True, header=True)
 
+
+def to_plot(num, dictionary, embs):
+    plot_embs = []
+    i = 0
+    while i < num:
+        plot_embs.append(embs[dictionary[i]])
+        i = i + 1
+    return plot_embs
+
+def load_dictionary(file_name):
+    dictionary = []
+    with open(file_name, "r") as f:
+        try:
+            while True:
+                for token in pickle.load(f):
+                    print(token)
+                    import unicodedata
+                    unicodedata.normalize('NFKD', token[0]).encode('ascii','ignore')
+                    dictionary.append(token[0])
+        except EOFError:
+            pass
+    return dictionary
 def write_txt(df):
     df.write.csv('preprocessed.csv')
 
@@ -76,8 +106,6 @@ def bucketize(df, field, min=-10, max=10, step=100):
     max = df.agg({field: "max"}).collect()[0][0]
     min = df.agg({field: "min"}).collect()[0][0]
     std = df.agg({field: "stddev"}).collect()[0][0]
-    #print (max)
-    #print (min)
     number_of_buckets = max - min / std
     buckets = np.arange(min, number_of_buckets, step, dtype=np.float).tolist()
     buckets = np.concatenate([[-float('inf')], buckets, [float('inf')]]).tolist()
@@ -143,28 +171,12 @@ for index, answer in enumerate(answers):
 '''
 
 
-
-
-
-#sqlContext = SQLContext(sc)
-
-#logsTest_rdd = sc.textFile("data/kddcup.data_10_percent_corrected").map(lambda line: line.split(","))
-
-#Employee_df = logsTest_rdd.toDF(schema)
-
-
-
-#word2Vec = Word2Vec(vectorSize=3, minCount=0, inputCol="duration,protocol_type,service,flag,src_bytes,dst_bytes,land,wrong_fragment,urgent,hot,num_failed_logins,logged_in,num_compromised,root_shell,su_attempted,num_root,num_file_creations,num_shells,num_access_files,num_outbound_cmds,is_host_login,is_guest_login,count,srv_count,serror_rate,srv_serror_rate,rerror_rate,srv_rerror_rate,same_srv_rate,diff_srv_rate,srv_diff_host_rate,dst_host_count,dst_host_srv_count,dst_host_same_srv_rate,dst_host_diff_srv_rate,dst_host_same_src_port_rate,dst_host_srv_diff_host_rate,dst_host_serror_rate,dst_host_srv_serror_rate,dst_host_rerror_rate,dst_host_srv_rerror_rate, anws", outputCol="result")
-#model = word2Vec.fit(Employee_df)
-#model.getVectors().show()
-
-
 # for counting unique values
 # #lstemp = [log[22] for log in logs]
 #temp = list(set(lstemp))
 #print(len(temp))
 
-def save_word2vec(vectors):
+def save_word2vec(vectors,dictionary):
     vecs_python = {}
     for key in vectors.keys():
         # print(key)
@@ -172,13 +184,42 @@ def save_word2vec(vectors):
     print('The vocabulary size is:' + str(len(vecs_python)))
 
     #np.array(vecs_python).dump(open('array.npy', 'wb'))
-    a = np.array(vecs_python)
+    #a = np.array(vecs_python)
+
     #np.set_printoptions(threshold=np.nan)
     #print(a)
     #for key in vectors.keys():
         #print("The key is:", key, "and the values are:", vecs_python[key] )
 
     word2vec_results_dict = {key : vecs_python[key] for key in vectors.keys()}
+    temp_X = word2vec_results_dict.values()
+
+    X = np.vstack(temp_X)
+    #y = y = np.hstack(vecs_python[key]
+                      #for key in vectors.keys())
+
+
+    #X_embedded = TSNE(n_components=2).fit_transform(X)
+    X_embedded = TSNE(random_state = RS).fit_transform(X)
+
+    #plot = to_plot(500, dictionary, word2vec_results_dict)
+    #tsne = TSNE(perplexity=100, n_iter=5000)
+    np.set_printoptions(suppress=True)
+    #Y = tsne.fit_transform(plot)
+
+    lol = []
+    for i in range(1000):
+        lol.append('')
+
+    plt.scatter(X_embedded[:, 0], X_embedded[:, 1])
+    for label, x, y in zip(lol[:500], X_embedded[:, 0], X_embedded[:, 1]):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    plt.show()
+    plt.scatter(X_embedded[:, 0], word2vec_results_dict[:, 1])
+    for label, x, y in zip(word2vec_results_dict[:500], X_embedded[:, 0], X_embedded[:, 1]):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+    plt.show()
+
 
     #with open('word2vec_dict.txt', 'wb') as handle:
         #wr = csv.writer(handle, quoting=csv.QUOTE_ALL)
@@ -192,6 +233,7 @@ def save_word2vec(vectors):
     #return vecs_python
 
 
+
 def Word2_vec():
     # Input data: Each row is a bag of words from a sentence or document.
 
@@ -203,27 +245,37 @@ def Word2_vec():
     ], ["text"])
 
 
-
-    #documentDF_temp.show()
-    # df = sqlContext.createDataFrame(logs_rdd, schema)
-    #listT = ["duration_bucketized_enc", "protocol_type", "service, flag", "src_bytes_enc", "dst_bytes_bucketized_enc", "land_enc, wrong_fragment_enc", "urgent_enc", "hot_enc", "num_failed_logins_enc", "logged_in_enc", "num_compromised_enc", "root_shell_enc", "su_attempted_enc", "num_root_enc", "num_file_creations_enc", "num_shells_enc", "num_access_files_enc", "num_outbound_cmds_enc", "is_host_login_enc", "is_guest_login_enc", "count_bucketized_enc", "srv_count_bucketized_enc", "serror_rate_bucketized_enc", "srv_serror_rate_bucketized_enc", "rerror_rate_bucketized_enc", "srv_rerror_rate_bucketized_enc", "same_srv_rate_bucketized_enc", "diff_srv_rate_bucketized_enc", "srv_diff_host_rate_enc", "dst_host_count_enc", "dst_host_srv_count_enc", "dst_host_same_srv_rate_enc", "dst_host_diff_srv_rate_enc", "dst_host_same_src_port_rate_enc", "dst_host_srv_diff_host_rate_enc", "dst_host_serror_rate_enc", "dst_host_srv_serror_rate_enc", "dst_host_rerror_rate_enc", "dst_host_srv_rerror_rate_enc"]
-    #word2Vec = Word2Vec(vectorSize=3, minCount=0, inputCol=(col for col in listT), outputCol="result")
-    #model = word2Vec.fit(dataframe_with_bucket)
-    #model.getVectors().show()
-
-    #dataframe_bucked_map = dataframe_with_bucket.rdd.map(lambda x: (x['duration_bucketized_enc'], x['protocol_type'],x['service'], x['flag'],x['src_bytes_enc'], x['dst_bytes_bucketized_enc'], x['land_enc'], x['wrong_fragment_enc'],x['urgent_enc'], x['hot_enc'],x['num_failed_logins_enc'], x['logged_in_enc'],x['num_compromised_enc'], x['root_shell_enc'], x['su_attempted_enc'],x['num_root_enc'], x['num_file_creations_enc'], x['num_shells_enc'],x['num_access_files_enc'], x['num_outbound_cmds_enc'], x['is_host_login_enc'], x['is_guest_login_enc'], x['count_bucketized_enc'], x['srv_count_bucketized_enc'],x['serror_rate_bucketized_enc'], x['srv_serror_rate_bucketized_enc'], x['rerror_rate_bucketized_enc'], x['srv_rerror_rate_bucketized_enc'], x['same_srv_rate_bucketized_enc'], x['diff_srv_rate_bucketized_enc'],x['srv_diff_host_rate_enc'], x['dst_host_count_enc'], x['dst_host_srv_count_enc'], x['dst_host_same_srv_rate_enc'],x['dst_host_diff_srv_rate_enc'], x['dst_host_same_src_port_rate_enc'],x['dst_host_srv_diff_host_rate_enc'], x['dst_host_serror_rate_enc'], x['dst_host_srv_serror_rate_enc'], x['dst_host_rerror_rate_enc'],x['dst_host_srv_rerror_rate_enc']))
-    #mtcars_map.take(5)
-    #rdd = dataframe_with_bucket.rdd
-
     word2vec = Word2Vec()
     word2vec.setNumPartitions(9).setVectorSize(100).setMinCount(1).setWindowSize(3).setLearningRate(0.00005)
     model = word2vec.fit(logs_rdd)
 
-    #word2vec_dict= save_word2vec(model.getVectors())
-    save_word2vec(model.getVectors())
-    #print(word2vec_dict.values())
+
     ###########################################
-    #wordCount = logs_rdd.flatMap(lambda line: line).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).sortBy(lambda x: -x[1])
+    wordCount = logs_rdd.flatMap(lambda line: line).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b).sortBy(lambda x: -x[1])
+    #wordCount.foreach(print)
+    frequent_words = wordCount.top(1000, key=None)
+    temp_dict1 = [i[0] for i in frequent_words]
+    temp_dict2 = [i[1] for i in frequent_words]
+
+    new_items = []
+    import unicodedata
+    for i in temp_dict1:
+        print (i)
+        unicodedata.normalize('NFKD', i).encode('ascii', 'ignore')
+        new_items.append(i)
+
+    print(new_items)
+    dictionaryFinal = dict(zip(temp_dict1, temp_dict2))
+    #print(dictionaryFinal)
+
+    #print(temp)
+
+    #with open('word_count_results.pickle', 'wb') as handle:
+        #pickle.dump(wordCount.collect(), handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    #dictionary = load_dictionary('word_count_results.pickle')
+    # word2vec_dict= save_word2vec(model.getVectors())
+    save_word2vec(model.getVectors(), dictionaryFinal)
     #with open("word2vec_dir.pickle" + '_vocab.pickle', 'wb') as handle:
         #pickle.dump(wordCount.collect(), handle, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -234,18 +286,6 @@ def Word2_vec():
 
 
     ##########################################
-    #dataframe_bucked_map.foreach(print)
-    #logs_rdd.foreach(print)
-    # Learn a mapping from words to Vectors.
-    #word2Vec = Word2Vec(vectorSize=100, minCount=0, inputCol=column_temp, outputCol="result")
-    #model = word2Vec.fit(dataframe_with_bucket)
-
-    #result = model.transform(df)
-    #for row in result.collect():
-        #text, vector = row
-        #print("Text: [%s] => \nVector: %s\n" % (", ".join(text), str(vector)))
-    #model.getVectors()
-
 
 
 Word2_vec()
