3e0fae4ef5f81538594fc7c0b608abd1c043a37a
Nick Kapetanas <nkapetanas@hotmail.com>
2018-05-29 01:41:53 +0300

    Keras neural netowork model updated - pending resolving fit issue

--- Keras_neural_network.py
+++ Keras_neural_network.py
@@ -1,9 +1,10 @@
 import numpy
 from keras import backend as K
-from keras.layers import Dense, Masking,Flatten, Convolution1D, MaxPooling1D, Flatten, concatenate
+import keras as k
+from keras.layers import Dense, Masking, Flatten, Convolution1D, MaxPooling1D, Flatten, concatenate
 from keras.models import Sequential
 from keras.layers import Dropout, Input, SpatialDropout1D
-from keras.layers.wrappers import TimeDistributed
+from keras.layers.wrappers import TimeDistributed, Bidirectional
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.regularizers import l2
@@ -25,11 +26,15 @@ DICTIONARY = "directory which contains the token:interger_code dictionary"
 MODEL_DIRECTORY = "directory with the neural network model"
 BATCH_SIZE = 512
 LEARNING_RATE = 0.0001
-VOCAB_SIZE = 5000
+NB_EPOCH = 50
+VERBOSE = 1
+VALIDATION_SPLIT = 0.2
+VOCAB_SIZE = 30886
 MAX_LENGTH = 42 # osa einai kai ta logs
-DROPOUT = 0.3
+DROPOUT_RATE = 0.3
 N_HIDDEN = 2 # einai to posa theloume na vgaloume
 PATH = r'C:/Users/Nick/PycharmProjects/IntrusionDetector/kdd_preprocessed.csv'  # use your path
+lr=0.001
 
 
 
@@ -41,12 +46,7 @@ class KerasNeuralNetwork():
     def load_data(self, allFiles, vocabulary):
         labels = []
         logs = []
-        '''
-        Sto vocabulary kane load to dictionary poy tha ftiakseis me spark, twra to afhsa keno apla gia na kanei compile
-        '''
-        # for k,v in vocabulary.items():
-        #     print(k,v)
-        # for f in os.listdir(dir):
+
         for f in allFiles:
             with open(f, 'r') as csvfile:
                 reader = csv.reader(csvfile)
@@ -54,59 +54,91 @@ class KerasNeuralNetwork():
                     # the line below transforms the tokens of a log entry to their corresponding integer values
                     # according to the dict dictionary
                     #row_to_integer = map(vocabulary.get, row)# kathe tokean sto antistixo integer
-                    print(row)
                     row_to_integer = list(map(vocabulary.get, row[0].split(",")))# kathe tokean sto antistixo integer
-                    print(row_to_integer)
                     logs.append(row_to_integer[:-1])
-                    labels.append(row_to_integer[:-1])
-        print(logs)
-        print(labels)
+                    labels.append(row_to_integer[-1])
+
+        """
+        
+        labels kai opou dei 28166 na valei 0 enw ean einai kati allo vale to 1
+
+        
+        """
         return logs, labels
 
 
     def model(self):
 
         # define the model
-        model = Sequential()
-        model.add(Embedding(VOCAB_SIZE, 8, input_length=MAX_LENGTH))
-        model.add(Dropout(DROPOUT))
-        # blstm tou ilia auto poy epistrefei prepei na einai dio output dld concatenated
-        model.add(Flatten())
-        model.add(Dropout(DROPOUT))
-        model.add(Dense(N_HIDDEN, activation='sigmoid'))
 
-        return model
+        # Sentence Input (word embeddings) Layer
+
+
+        #model.add(Embedding(VOCAB_SIZE, 80 ))
+        #model.add(Dropout(DROPOUT))
+
+        # word_embeddings = Embedding(VOCAB_SIZE, 80)
+        word_embeddings = Input(shape=(VOCAB_SIZE,80), name='input_layer_words')
+
+        # Droupout over word embegdings
+        noise_log_embeddings = SpatialDropout1D(DROPOUT_RATE)(word_embeddings)
+
+        #sentence_embedding = Dropout(DROPOUT_RATE)(word_embeddings)
+
+        BLSTM = k.layers.Bidirectional(k.layers.LSTM(200, activation='tanh', recurrent_activation='hard_sigmoid', use_bias=True, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', unit_forget_bias=True, dropout=0.3, recurrent_dropout=0.3, implementation=1,stateful=False,  return_state=False, go_backwards=False), merge_mode='concat', weights=None)(noise_log_embeddings)
+        blstm_dropout = Dropout(DROPOUT_RATE)(BLSTM)
+        outputs = Dense(N_HIDDEN, activation='softmax')(blstm_dropout)
+
+        # Log Input Layer (Sentences, Words, Word Embeddings' Dimensionality)
+        log_inputs = Input([VOCAB_SIZE, 80], name='log_inputs')
+
+
+
+        # Wrap up Distributed Sentences Network
+        self._model = Model(inputs=word_embeddings, outputs=[outputs])
+
+        self._model.compile(optimizer=Adam(lr=lr, clipvalue=5.0),
+                            loss='binary_crossentropy', metrics=['accuracy'])
+
+        print(self._model.summary())
+
+        return self._model
 
     def train(self, vocabulary):
 
-
         allFiles = glob.glob(PATH + "/*.csv")
+
         logs, labels = self.load_data(allFiles, vocabulary)
-        # split into input (X) and output (Y) variables
-        #X = dataset[:, 0:8]
-        #Y = dataset[:, 8]
-        # we split our data as batches
+
         model = self.model()
+        # we split our data as batches
+
         batches = len(logs)//BATCH_SIZE
         training_logs = np.array_split(logs, batches)
         training_labels = np.array_split(labels, batches)
 
         # call the model for training
         # compile the model
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+        #model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
         # summarize the model
-        print(model.summary())
+
         # fit the model
-        model.fit(training_logs, training_labels, epochs=50, verbose=0) # kane to modelo save
+        fit_model_result = model.fit(training_logs, training_labels, batch_size = BATCH_SIZE, epochs = NB_EPOCH, verbose = VERBOSE, validation_split = VALIDATION_SPLIT)
+
+        return model
+
+    def test(self, model):
+
+        all_files = glob.glob("KDD_Test_Data_Preprocessed.csv/*.csv")
+
+        test_logs, test_labels = None #self.load_data(all_files, vocabulary)
+
+        model = self.model()
+
         # evaluate the model
-        loss, accuracy = model.evaluate(training_logs, training_labels, verbose=0)
-        print('Accuracy: %f' % (accuracy*100))
+        loss, accuracy = model.evaluate(test_logs, test_labels, verbose=VERBOSE)
+        print('Accuracy: %f' % (accuracy * 100))
 
-    def test(self):
-
-    # tha paroume to modelo apo panw tou fit kai tha tou perasoume ta test dedomena test_logs kai test labels
-    # edw pali to idio me panw alla adi na kaleseis to train operation toy grafou
-    # kaleis to test
         return
 
     def main(self):
